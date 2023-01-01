37e17dd634cbd10123ce1225e6db09b97dad0a68
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2023-01-01 16:15:34 +0100

    Add error handling

--- get_weather.js
+++ get_weather.js
@@ -9,12 +9,17 @@ getCityBtn.addEventListener('click', () => {
 });
 
 async function getWeather(cityName) {
-    const response = await fetch (`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`);
-    const weatherData = await response.json();
+    try {
+        const response = await fetch (`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`);
+        const weatherData = await response.json();
+        console.log(weatherData);
+        data.innerHTML = `<h3>City: ${weatherData.name}</h3>
+                        <p>Temperature: ${weatherData.main['temp']}</p>
+                        <p>Weather: ${weatherData.weather[0]['description']}</p>`;
+    } catch (err) {
+        data.innerHTML = '<h3 style="color:red">Something went wrong, check console for details...</h3>';
+         console.error(err);
+    }
 
-    console.log(weatherData);
-    console.log(weatherData.main['temp']);
-    console.log(weatherData.weather[0]['description']);
 
-    data.innerHTML = `<h3>City: ${weatherData.name}</h3><p>Temperature: ${weatherData.main['temp']}</p><p>Weather: ${weatherData.weather[0]['description']}</p>`;
 }
