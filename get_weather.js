9c59877fc0cbedb7b6572fb202fcf7ee3c283791
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2023-01-01 17:26:25 +0100

    Add some more weather info like min./max. temperature, humidity, cloudiness, wind speed...

--- get_weather.js
+++ get_weather.js
@@ -10,16 +10,22 @@ getCityBtn.addEventListener('click', () => {
 
 async function getWeather(cityName) {
     try {
-        const response = await fetch (`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`);
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`);
         const weatherData = await response.json();
-        console.log(weatherData);
-        data.innerHTML = `<h3>City: ${weatherData.name}</h3>
-                        <p>Temperature: ${weatherData.main['temp']}</p>
-                        <p>Weather: ${weatherData.weather[0]['description']}</p>`;
+        data.innerHTML = 
+            `<h3>City: ${weatherData.name}</h3>
+            <p>Temperature: ${weatherData.main['temp']}</p>
+            <p>Feels like: ${weatherData.main['feels_like']}</p>
+            <p>Min. temperature: ${weatherData.main['temp_min']}</p> 
+            <p>Max. temperature: ${weatherData.main['temp_max']}</p>
+            <p>Humidity: ${weatherData.main['humidity']}%</p>
+            <p>Cloudiness: ${weatherData.clouds['all']}%</p>
+            <p>Wind speed: ${weatherData.wind['speed']} m/s</p>
+            <p>Weather: ${weatherData.weather[0]['description']}</p>
+            <img src="http://openweathermap.org/img/wn/${weatherData.weather[0]['icon']}@2x.png" />
+            `
     } catch (err) {
         data.innerHTML = '<h3 style="color:red">Something went wrong, check console for details...</h3>';
-         console.error(err);
+        console.error(err);
     }
-
-
 }
