db9bae9df53ab87e9b19dc8ee8537185c5e3aba6
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2023-01-01 18:09:27 +0100

    Split the weather data into two different data columns

--- get_weather.js
+++ get_weather.js
@@ -1,8 +1,17 @@
 import apiKey from "./key.js";
 
+// Form
 const city = document.getElementById('city');
 const getCityBtn = document.getElementById('get-city-btn');
+
+// Data section
 const data = document.getElementById('data');
+const temperatureData = document.getElementById('temperature-data');
+const otherData = document.getElementById('other-data');
+
+// City name
+const cityNameHeading = document.createElement('h3');
+cityNameHeading.setAttribute('id', 'city-name')
 
 getCityBtn.addEventListener('click', () => {
     getWeather(city.value);
@@ -12,18 +21,20 @@ async function getWeather(cityName) {
     try {
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`);
         const weatherData = await response.json();
-        data.innerHTML = 
-            `<h3>City: ${weatherData.name}</h3>
-            <p>Temperature: ${weatherData.main['temp']}</p>
-            <p>Feels like: ${weatherData.main['feels_like']}</p>
-            <p>Min. temperature: ${weatherData.main['temp_min']}</p> 
-            <p>Max. temperature: ${weatherData.main['temp_max']}</p>
-            <p>Humidity: ${weatherData.main['humidity']}%</p>
-            <p>Cloudiness: ${weatherData.clouds['all']}%</p>
-            <p>Wind speed: ${weatherData.wind['speed']} m/s</p>
-            <p>Weather: ${weatherData.weather[0]['description']}</p>
-            <img src="http://openweathermap.org/img/wn/${weatherData.weather[0]['icon']}@2x.png" />
-            `
+
+        cityNameHeading.innerText = `City: ${weatherData.name}`;
+        data.insertAdjacentElement('beforebegin', cityNameHeading);
+        
+        temperatureData.innerHTML = `<p>Temperature: ${weatherData.main['temp']}</p>
+        <p>Feels like: ${weatherData.main['feels_like']}</p>
+        <p>Min. temperature: ${weatherData.main['temp_min']}</p> 
+        <p>Max. temperature: ${weatherData.main['temp_max']}</p>`;
+
+        otherData.innerHTML = `<p>Humidity: ${weatherData.main['humidity']}%</p>
+        <p>Cloudiness: ${weatherData.clouds['all']}%</p>
+        <p>Wind speed: ${weatherData.wind['speed']} m/s</p>
+        <p>Weather: ${weatherData.weather[0]['description']}</p>
+        <img src="http://openweathermap.org/img/wn/${weatherData.weather[0]['icon']}@2x.png" />`
     } catch (err) {
         data.innerHTML = '<h3 style="color:red">Something went wrong, check console for details...</h3>';
         console.error(err);
